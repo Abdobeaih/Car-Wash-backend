51b8aee5ffd2c5e7f690d138606c48b2c8fd372c
Abdobeaih <abdobeaih43@gmail.com>
2026-09-12 21:06:23 +0200

    refactor(auth): update confirmPassword field to use Allow decorator in RegisterDto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsEnum, IsOptional, IsString, Matches, MinLength } from 'class-validator';
+import { Allow, IsEmail, IsEnum, IsOptional, IsString, Matches, MinLength } from 'class-validator';
 import { UserRole } from '../../common/constants/roles';
 import { OtpChannel } from '../../otp/schemas/otp.schema';
 
@@ -14,9 +14,9 @@ export class RegisterDto {
   @MinLength(8, { message: 'Password must be at least 8 characters' })
   password: string;
 
-  @IsOptional()
-  @IsString()
-  confirmPassword: string;
+  @Allow()
+@IsString()
+confirmPassword: string;
 
   @IsOptional()
   @IsString()
